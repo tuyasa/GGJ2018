d97506dcd5dc3ab95d0325e91cb0c0bf6fd89edb
Gregoire Peltier <idevedit@gmail.com>
2018-01-27 23:15:41 +0100

    Readded the collider margin

--- GGJ2018Project/Assets/Scripts/Controllable/Controller.cs
+++ GGJ2018Project/Assets/Scripts/Controllable/Controller.cs
@@ -19,7 +19,7 @@ public class Controller : MonoBehaviour
 	public bool IsGrounded()
 	{
 		Collider2D _collider = GetComponent<Collider2D>();
-		RaycastHit2D hit = Physics2D.Raycast(_transform.position+Vector3.down*_collider.bounds.extents.y, Vector2.down, 0.02f,ground_layers);
+		RaycastHit2D hit = Physics2D.Raycast(_transform.position+Vector3.down*_collider.bounds.extents.y, Vector2.down, margin,ground_layers);
 		
 		return hit.collider!=null;
 
