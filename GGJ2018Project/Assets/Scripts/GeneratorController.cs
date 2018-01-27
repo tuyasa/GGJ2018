a79e1484b65a825494e09e134e02f7ab135e19bc
Gregoire Peltier <idevedit@gmail.com>
2018-01-28 00:11:02 +0100

    Added the Generator Controller

---
+++ GGJ2018Project/Assets/Scripts/GeneratorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorController : MonoBehaviour
+{
+	
+	private SpriteRenderer _renderer;
+	public Sprite Off;
+	public Sprite On;
+	private void Awake()
+	{
+		_renderer = GetComponentInChildren<SpriteRenderer>();
+	}
+
+	public void Switch()
+	{
+		Debug.Log("paf");
+		_renderer.sprite = Off;
+		Invoke("SwitchOn",1);
+	}
+
+	private void SwitchOn()
+	{
+		_renderer.sprite = On;
+	}
+}
