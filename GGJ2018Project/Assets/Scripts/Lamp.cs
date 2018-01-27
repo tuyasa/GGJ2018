64089993eca1a04667654859c45896879d76ec86
tuyasa <tuyasa@live.fr>
2018-01-27 23:33:18 +0100

    unity ggj menu template + moveobject

---
+++ GGJ2018Project/Assets/Scripts/Lamp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+	public Sprite activate;
+	public Sprite deactivate;
+
+	private SpriteRenderer _spriteRenderer;
+
+	private void Awake()
+	{
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DesactivateLamp()
+	{
+		_spriteRenderer.sprite = deactivate;
+	}
+
+	public void ActiveLamp()
+	{
+		_spriteRenderer.sprite = activate;
+	}
+}
