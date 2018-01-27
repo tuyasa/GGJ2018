fe66befe8a5c4039e463d2ca66feec85b2e3e772
Gregoire Peltier <idevedit@gmail.com>
2018-01-27 12:09:00 +0100

    refactored controllable scripts

---
+++ GGJ2018Project/Assets/Scrips/Controllable/Controller.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Controllable : MonoBehaviour
+{
+	//true if the controlled element is a jumper
+	public bool jumper= false;
+	//movement speed for horizontal movement 
+
+	//set the jump force if a jumper.
+	
+	
+	//next possible jump 
+
+
+
+	private void Awake()
+	{
+		
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	
+	private void FixedUpdate()
+	{
+		
+		
+		Vector2 rigidMovement  = new Vector2(hMove,vMove);
+
+		_rgbd.MovePosition(_rgbd.position+rigidMovement );
+
+		if ( Time.time > nextJumpTime )
+		{
+			
+			float jump = Input.GetAxis("BioJump")*jumpForce;
+			nextJumpTime = Time.time + jump_cooldown;
+			_rgbd.AddForce(new Vector2(0,jump),ForceMode2D.Impulse);
+
+		}
+		
+	}
+}
