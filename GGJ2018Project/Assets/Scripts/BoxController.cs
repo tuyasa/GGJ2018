4063f33feb2c99f2114b3c80ced2adacbebf10bc
Gregoire Peltier <idevedit@gmail.com>
2018-01-28 13:35:25 +0100

    Added the Box Prefab and Fauteuil, and sounds

---
+++ GGJ2018Project/Assets/Scripts/BoxController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    public GameObject Cat;
+    private Character _character;
+    private SwitchSpriteController _switch;
+    private void Awake()
+    {
+        _character = GetComponent<Character>();
+        _switch = GetComponent<SwitchSpriteController>();
+    }
+
+    public void Capture()
+    {
+        _character.Host();
+        _switch.SwitchSprite();
+        Destroy(Cat);
+    }
+}
