54b2a6dcafab9c094319bdb54a7092d1407249b9
tuyasa <tuyasa@live.fr>
2018-01-28 13:17:14 +0100

    test scene raph

---
+++ GGJ2018Project/Assets/Scripts/Windows.cs
@@ -0,0 +1,47 @@
+﻿using Managers;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Windows : MonoBehaviour
+{
+	public AudioClip AudioClip;
+	public UnityEvent UnityEvent;
+	public SwitchSpriteController LampSwitchSpriteController;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag.Equals("Human"))
+		{
+			Debug.Log("command event");
+			Character character = other.GetComponent<Character>();
+			character._action = true;
+			// lamp activate
+			if (LampSwitchSpriteController.index == 1)
+				UnityEvent.Invoke();
+			else
+			{
+				LevelManager.Instance.ChangeSceneTo(0);
+			}
+			SoundManager.Instance.PlaySoundSFX(AudioClip);
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.tag.Equals("Human"))
+		{
+			Debug.Log("command event");
+			Character character = other.GetComponent<Character>();
+			character._action = false;
+		}
+	}
+}
