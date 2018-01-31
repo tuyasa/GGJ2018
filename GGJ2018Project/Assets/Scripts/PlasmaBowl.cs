f9b372f3e3a737842f9f7056ba3827e9b3242a98
tuyasa <tuyasa@live.fr>
2018-02-01 00:27:22 +0100

    plasma bowl sound

--- GGJ2018Project/Assets/Scripts/PlasmaBowl.cs
+++ GGJ2018Project/Assets/Scripts/PlasmaBowl.cs
@@ -5,16 +5,19 @@ public class PlasmaBowl : MonoBehaviour
     public Sprite inactiveSprite;
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
+    private AudioSource _audioSource;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     public void Disable()
     {
         _animator.enabled = false;
         _spriteRenderer.sprite = inactiveSprite;
+        _audioSource.Stop();
     }
 }
