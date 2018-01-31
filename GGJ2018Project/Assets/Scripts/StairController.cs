dc6206b578b6fefd2677f70857e5b7f6617bc04b
Mendrock <gael.baurens@gmail.com>
2018-01-31 22:17:47 +0100

    Maj du Menu Gael

--- GGJ2018Project/Assets/Scripts/StairController.cs
+++ GGJ2018Project/Assets/Scripts/StairController.cs
@@ -24,7 +24,7 @@ public class StairController : MonoBehaviour
 	{
 		if (CheckEndLevel1Conditions(other))
 		{
-			LevelManager.Instance.ChangeSceneTo(0);
+			LevelManager.Instance.ChangeSceneTo(2);
 		}
 	}
 
@@ -32,7 +32,7 @@ public class StairController : MonoBehaviour
 	{
 		if (CheckEndLevel1Conditions(other))
 		{
-			LevelManager.Instance.ChangeSceneTo(0);
+			LevelManager.Instance.ChangeSceneTo(2);
 		}
 	}
 }
